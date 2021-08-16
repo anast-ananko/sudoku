9eca5b9e7a1f62843655fe87bb1e3bd51c35f9ac
anast-ananko <anast.ananko@gmail.com>
2021-08-16 19:53:18 +0300

    feat: complete sudoku task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  
+  const size = 9;
+  const box = 3;
+
+  const findEmpty = (matrix) => {
+      for (let i = 0; i < size; i++) {
+          for (let j = 0; j < size; j++) {
+              if(matrix[i][j] === 0) {
+                  return [i,j];
+              }
+          }
+      }
+      return null;
+  }
+
+  const check = (num, pos, matrix) => {
+      const [r,c] = pos;
+
+      for (let i = 0; i < size; i++) {
+          if (matrix[i][c] === num && i !== r) {
+              return false;
+          }
+      }
+
+      for (let i = 0; i < size; i++) {
+          if (matrix[r][i] === num && i !== c) {
+              return false;
+          }
+      }
+
+      const boxRow = Math.floor( r/box ) * box;
+      const boxCol = Math.floor( c/box ) * box;
+
+      for (let i = boxRow; i < boxRow + box; i++) {
+          for (let j = boxCol; j < boxCol + box; j++) {
+              if (matrix[i][j] === num && i !== r && j !== c) {
+                  return false;
+              }
+          }
+      }
+      return true;
+  }
+
+  const solveMatrix = () => { 
+    const currPos = findEmpty(matrix);
+
+    if (currPos === null) {
+        return true;
+    }
+    for (let i = 1; i < size + 1; i++) {
+        const currNum = i;
+        const isValid = check(currNum, currPos, matrix);
+
+        if (isValid) {
+             const [x,y] = currPos;
+             matrix[x][y] = currNum;
+
+            if(solveMatrix()) {
+                 return true;
+            }
+
+             matrix[x][y] = 0;
+         }
+     }
+
+    return false;
+   
+  }
+
+  solveMatrix();
+  return matrix;
 }
